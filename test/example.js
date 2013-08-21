bab6d4709c483e84912187879b39d8ae546d29d5
Seldo <github@seldo.com>
2013-08-20 20:01:54 -0700

    Working out how models will work, slowly

--- test/example.js
+++ test/example.js
@@ -1,4 +1,17 @@
 /**
- An example model to test out how things work with adapters
+ * Testing how models will work.
  */
+
+// this bit normally happens in app.js
+var env = 'makomi' // production | test
+
+// load the config
+var mkSrc = require('makomi-source-utils');
+mkSrc.config.setConfigFileLocation('./testapp1/.makomi/config.json')
+mkSrc.config.loadConfig(env)
+
+// load the datasource and connect()
+
+adapter.connect()
+
 
